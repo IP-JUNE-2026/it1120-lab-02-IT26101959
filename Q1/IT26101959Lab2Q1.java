799dd3cd85e0753a1aa70bd9ac6ef0117f4229e3
IT26101959 <IT26101959@my.sliit.lk>
2026-06-16 09:29:07 +0530

    Add files via upload

---
+++ Q1/IT26101959Lab2Q1.java
@@ -0,0 +1,11 @@
+public class IT26101959Lab2Q1 {
+    public static void main(String[] args) {
+
+        double perimeter = 100;
+        double length = 200.0 / 7.0;
+        double width = 150.0 / 7.0;
+
+        System.out.println("Length of the fence: " + length);
+        System.out.println("Width of the fence: " + width);
+    }
+}
